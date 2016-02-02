56b36c10cb891b3d3132a0883be73dbbe42e84a7
hccadmin <dave_rothfarb@dfci.harvard.edu>
2016-02-02 18:41:46 -0500

    Styled staff, news, starting to tackle questionnaires

--- sites/all/themes/labtheme/templates/node.tpl.php
+++ sites/all/themes/labtheme/templates/node.tpl.php
@@ -84,7 +84,9 @@
   <div class="title-container">
     <?php print render($title_prefix); ?>
     <?php if (!$page && !empty($title)): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <!--
+    <h2<?php //print $title_attributes; ?>><a href="<?php //print $node_url; ?>"><?php //print $title; ?></a></h2>
+      -->
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php if ($display_submitted): ?>
